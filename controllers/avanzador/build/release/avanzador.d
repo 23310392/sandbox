dec48adcc65ba6fcf9781860e0478cd6506adeb1
23310392 <a23310392@ceti.mx>
2026-09-18 23:05:00 -0600

    Gg

---
+++ controllers/avanzador/build/release/avanzador.d
@@ -0,0 +1,5 @@
+build/release/avanzador.o: avanzador.cpp \
+ C:\Users\Dan\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:/Users/Dan/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\Dan\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\Dan\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp
---
+++ controllers/controlador_pendulo/build/release/controlador_pendulo.d
@@ -0,0 +1,6 @@
+build/release/controlador_pendulo.o: controlador_pendulo.cpp \
+ C:\Users\Dan\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:/Users/Dan/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\Dan\AppData\Local\Programs\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ C:\Users\Dan\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:\Users\Dan\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp
